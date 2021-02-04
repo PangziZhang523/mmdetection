8dc546df2c45f181c2e296f1d88586cbf5070319
PangziZhang523 <hhaoshine@163.com>
2021-02-04 21:49:15 +0800

    weight 7--->9 class

--- tools/model_converters/weight.sh
+++ tools/model_converters/weight.sh
@@ -7,4 +7,6 @@
  # @FilePath: /mmdetection/tools/train.sh
 ### 
 set -ex
-python /data/train_my/mmdetection/tools/model_converters/weight.py --org_path /data/train_my/mmdetection/weights/cascade_rcnn_r50_fpn_1x_coco_20200316-3dc56deb.pth --num_classes 7 
+python /data/train_my/mmdetection/tools/model_converters/weight.py \
+--org_path /data/train_my/mmdetection/weights/cascade_rcnn_r50_fpn_1x_coco_20200316-3dc56deb.pth \
+--num_classes 9
