5f22c187322231977760e74ce028f1f4d6344f1e
PangziZhang523 <hhaoshine@163.com>
2021-02-05 03:59:56 +0000

    back

--- tools/test.py
+++ tools/test.py
@@ -20,7 +20,7 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='MMDet test (and eval) a model')
     parser.add_argument('config', help='test config file path')
-    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument('checkpoint', default='/data_raid5_21T/zgh/ZGh/work_dirs/cascade_r2_3/latest.pth' , help='checkpoint file')
     parser.add_argument('--out', help='output result file in pickle format')
     parser.add_argument(
         '--fuse-conv-bn',
